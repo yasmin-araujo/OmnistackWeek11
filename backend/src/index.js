92856f67555c8fe6ab89549cee0edddf6cf05b95
yasmin_araujo <yasminoa@hotmail.com>
2020-03-24 23:45:01 -0300

    Update backend
    - Add knex
    - Create database
    - Create controllers
    - Manage routes
    - Add cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
- 
-app.get('/', (request, response) => {
-    return response.send('Ola');
-});
+app.use(routes);
 
 app.listen(3333);
